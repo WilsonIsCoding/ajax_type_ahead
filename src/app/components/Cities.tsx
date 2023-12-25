241695b56c68db79601a1cb58744f480ca38cb21
WilsonIsCoding <wilsoncheng0704.wc@gmail.com>
2023-12-25 13:53:26 +0800

    [✨ feat]：project init

---
+++ src/app/components/Cities.tsx
@@ -0,0 +1,22 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+export default function Coins({ cities }) {
+  return (
+    <>
+      <ul className="grid grid-cols-4 mx-auto max-w-[1260px] gap-10">
+        {cities.map((city) => (
+          <li key={city.latitude}>
+            <span className="name">
+              ${city.city}, ${city.state}
+            </span>
+            <span className="population">
+              ${numberWithCommas(city.population)}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ src/app/components/SearchCoins.tsx
@@ -0,0 +1,45 @@
+"use client";
+export default function SearchCoins({ getSearchResults }) {
+  const handlerOnChange = (value) => {
+    getSearchResults(value);
+  };
+
+  return (
+    <div className="text-center my-20">
+      <div className="relative z-10">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </div>
+        <label
+          htmlFor="search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Search something
+        </label>
+        <input
+          type="search"
+          placeholder="Search something"
+          required
+          onChange={(e) => {
+            handlerOnChange(e.target.value);
+          }}
+          className={`block w-full outline-0 p-4 pl-10 text-sm text-gray-900 bg-white `}
+        />
+      </div>
+    </div>
+  );
+}
